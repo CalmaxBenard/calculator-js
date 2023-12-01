92738f1c722184a9f1abd83fa0582df2428f4076
CalmaxBenard <calmaxbenard@gmail.com>
2023-12-01 18:34:29 +0300

    link scripts.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defualtResult = 0;
+let currentResult = defualtResult;
+
+currentResult += (10 * 3) / 2;
+
+let calculationDescription = `(${defualtResult} + 10) * 3 / 2`;
+
+outputResult(currentResult, calculationDescription);
