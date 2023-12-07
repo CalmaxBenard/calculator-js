2f4b5da6e5662a55dd2816d70ee5e54b366c0ad1
CalmaxBenard <calmaxbenard@gmail.com>
2023-12-07 19:52:13 +0300

    add other operations and refactor code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,44 @@
 const defualtResult = 0;
 let currentResult = defualtResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteLog(operator, resultBeforeCalculation, calcNumber){
+    const calcDescription = `${resultBeforeCalculation} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
+}
+
 function add() {
-  currentResult += parseInt(userInput.value);
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult
+  currentResult += enteredNumber;
+  createAndWriteLog("+", initialResult, enteredNumber)
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult
+  currentResult -= enteredNumber;
+  createAndWriteLog("-", initialResult, enteredNumber)
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult
+  currentResult *= enteredNumber;
+  createAndWriteLog("x", initialResult, enteredNumber)
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult
+  currentResult /= enteredNumber;
+  createAndWriteLog("/", initialResult, enteredNumber)
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract)
+multiplyBtn.addEventListener("click", multiply)
+divideBtn.addEventListener("click", divide)
